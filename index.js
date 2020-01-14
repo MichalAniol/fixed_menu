27272803a614f3dbc84890dbd11dde97ae60f824
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-01-14 22:05:11 +0100

    2nd

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const imgH = document.querySelector('.image').getBoundingClientRect().height;
 
-const menu = document.querySelectorAll('.menu')[0];
+window.onload = () => {
+    const imgH = document.querySelector('.image').getBoundingClientRect().height,
+        menu = document.querySelectorAll('.menu')[0],
+        guy = document.querySelectorAll('.guy')[0],
+        text = document.querySelectorAll('.image .text')[0];
 
-var menuFixed = false;
+    var menuFixed = false;
 
-window.addEventListener('scroll', (e) => {
-    if (window.scrollY > imgH) {
-        if (!menuFixed) {
-            menu.style.position = 'fixed';
-            menu.style.top = '0';
-            menuFixed = true;
-            console.log('%c menuFixed:', 'background: #ffcc00; color: #003300', menuFixed)
+    text.style.fontSize = (window.innerWidth / 24) + 'px';
+    window.addEventListener("resize", () => {
+        text.style.fontSize = (window.innerWidth / 24) + 'px';
+    });
+
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > imgH) {
+            if (!menuFixed) {
+                menu.style.position = 'fixed';
+                menu.style.top = '0';
+                menuFixed = true;
+                console.log('%c menuFixed:', 'background: #ffcc00; color: #003300', menuFixed)
+            }
+        } else {
+            if (menuFixed) {
+                menu.style.position = 'relative'
+                menu.style.top = 'auto'
+                menuFixed = false;
+                console.log('%c menuFixed:', 'background: #ffcc00; color: #003300', menuFixed)
+            }
+            guy.style.top = (window.scrollY / 1.8) + 'px';
+            text.style.top = ((imgH + window.scrollY) / 2) + 'px';
         }
-    } else {
-        if (menuFixed) {
-            menu.style.position = 'relative'
-            menu.style.top = 'auto'
-            menuFixed = false;
-            console.log('%c menuFixed:', 'background: #ffcc00; color: #003300', menuFixed)
-        }
-    }
-})
+    })
+}
